8041dcd29f0879fe3644edebc7b6eafcec0a8356
Araozu (woo) <faraoz@unsa.edu.pe>
2020-04-20 15:43:48 -0500

    Cambiada la forma en la que funcionanlas funciones del modulo Expect.

--- AnalisisSintactico/Expectations.fs
+++ AnalisisSintactico/Expectations.fs
@@ -29,69 +29,64 @@ let Any resLexer msgError fnErrorLexer fnEOF =
 
 let TNuevaLinea resLexer valorOpc msgError =
     let preToken = extraerToken resLexer msgError
-    try
-        let (TNuevaLinea infoToken) = preToken
+    match preToken with
+    | TNuevaLinea infoToken ->
         match valorOpc with
         | Some v ->
             if infoToken.valor = v then infoToken
             else failwith ""
         | None -> infoToken
-
-    with
-    | _ -> failwith msgError
+    | _ ->
+        failwith msgError
 
 
 let TIdentificador resLexer valorOpc msgError =
     let preToken = extraerToken resLexer msgError
-    try
-        let (TIdentificador infoToken) = preToken
+    match preToken with
+    | TIdentificador infoToken ->
         match valorOpc with
         | Some v ->
             if infoToken.valor = v then infoToken
             else failwith ""
         | None -> infoToken
 
-    with
     | _ -> failwith msgError
 
 
 let PC_SEA resLexer valorOpc msgError =
     let preToken = extraerToken resLexer msgError
-    try
-        let (PC_SEA infoToken) = preToken
+    match preToken with
+    | PC_SEA infoToken ->
         match valorOpc with
         | Some v ->
             if infoToken.valor = v then infoToken
             else failwith ""
         | None -> infoToken
 
-    with
     | _ -> failwith msgError
+
 
 let PC_MUT resLexer valorOpc msgError =
     let preToken = extraerToken resLexer msgError
-    try
-        let (PC_MUT infoToken) = preToken
+    match preToken with
+    | PC_MUT infoToken ->
         match valorOpc with
         | Some v ->
             if infoToken.valor = v then infoToken
             else failwith ""
         | None -> infoToken
 
-    with
     | _ -> failwith msgError
-
 
 
 let TOperador resLexer valorOpc msgError =
     let preToken = extraerToken resLexer msgError
-    try
-        let (TOperador infoToken) = preToken
+    match preToken with
+    | TOperador infoToken ->
         match valorOpc with
         | Some v ->
             if infoToken.valor = v then infoToken
             else failwith ""
         | None -> infoToken
 
-    with
     | _ -> failwith msgError
